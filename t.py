f28770f7fc76fe833daa7b44cc93fbcd0a2875b6
Omar EL FILALI <omarelfiie2007@gmail.com>
2026-09-04 16:54:44 +0100

    ;

--- t.py
+++ t.py
@@ -1,2 +1,14 @@
-def fusionner_dictionnaire(dict1, dict2, func):
-    return { k: func(dict1[k], dict2[k]) if k in dict1 and k in dict2 else dict1.get(k, dict2.get(k)) for k in dict1.keys() | dict2.keys() }
+def moyenne_poderee(valeurs, poids):
+    mult = []
+    for i, j in zip(l, s):
+        mult.append(i*j)
+    
+    return (sum(mult))/sum(poids)
+
+l = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+s = [9, 2, 3, 4, 5, 6, 7, 8, 9]
+    
+# print(temp)
+print(moyenne_poderee(l ,l))
+
+
